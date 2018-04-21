092a73190bd8fa72f1ecc377090342e8d25769a9
Henry Mesias <henrymesias@gmail.com>
2018-04-21 11:53:48 -0400

    added scss to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './App.css';
+import './Custom.css';
 
 class App extends Component {
   render() {
