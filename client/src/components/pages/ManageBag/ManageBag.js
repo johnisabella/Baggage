37afd991cf60299ddba4c89bc848553225060ea2
Henry Mesias <henrymesias@gmail.com>
2018-04-25 21:20:28 -0400

    fixed issues

--- client/src/components/pages/ManageBag/ManageBag.js
+++ client/src/components/pages/ManageBag/ManageBag.js
@@ -8,16 +8,12 @@ class ManageBag extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <p> Manage Bag Page</p>
-=======
       <div>
       <Header />
       <ChildPageHeader />
       <ChildPageMainContainer />
       <p> Manage Bag Page Test Text </p>
       </div>
->>>>>>> fe54d261b7c666815f8a8886f7a13e3923401186
       );
     }
   }
--- client/src/components/structure/MainContainer/MainContainer.js
+++ client/src/components/structure/MainContainer/MainContainer.js
@@ -9,7 +9,3 @@ const MainContainer = props => (
 
 export default MainContainer;
 
-<<<<<<< HEAD
-//JI updated file name to camel case 4/25
-=======
->>>>>>> fe54d261b7c666815f8a8886f7a13e3923401186
